72cd70149e2c3c0700bfd7916d21395ac39acf10
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2025-04-03 02:41:17 -0400

    updated jumper code

--- CPU16.srcs/sources_1/new/PCUpdate.v
+++ CPU16.srcs/sources_1/new/PCUpdate.v
@@ -6,8 +6,7 @@ module ProgramCounterUpdate(
     input [15:0] instruction,   // instruction; contains immediate address, and the opcode.
     input branch_zero           // should be 0 for beq, and 1 for bne
     );
-    
-    // TODO implement sign extension for jumps on the jmp_address line
+
     // TODO test this module
     
     wire [3:0] opcode = instruction[15:12];
@@ -15,15 +14,15 @@ module ProgramCounterUpdate(
     reg [11:0] jmp_address;
     
     always @(*) begin
-        immediate <= {11'h0, instruction[3:0] << 1};
+        immediate <= {{11{instruction[3]}}, instruction[3:0] << 1};     // sign-extended immediate value
         jmp_address <= {3'h0, instruction[11:0] << 1};
-        if (opcode == 4 && branch_zero == 0) begin // beq
-            pc_next <= pc + immediate;
+        if (opcode == 4 && branch_zero == 0) begin          // beq
+            pc_next <= pc + immediate + 2;
         end else if (opcode == 5 && branch_zero == 1) begin // bne
-            pc_next <= pc + immediate;
-        end else if (opcode == 6) begin
-            pc_next <= pc + jmp_address;
-        end else begin
+            pc_next <= pc + immediate + 2;
+        end else if (opcode == 6) begin                     // jmp
+            pc_next <= pc + { {4{jmp_address[11]}}, jmp_address} + 2;   // sign-extended jump address
+        end else begin                                      // advance program counter; regular case
             pc_next <= pc + 2;
         end
     end
